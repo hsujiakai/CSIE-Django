079a1c81f6ca898d4d322b7fcdecc65024948163
Alexander <alexanderinnow@gmail.com>
2020-05-09 16:16:04 +0800

    update Day01

--- myblog/mainsite/views.py
+++ myblog/mainsite/views.py
@@ -1,3 +1,23 @@
+from django.template.loader import get_template
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
 
 # Create your views here.
+
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals()) # locals() => 存成物件
+    return HttpResponse(html)
+
+
+# def homepage(request):
+    # posts = Post.objects.all()
+    # post_lists = list() #! [] 寫成這樣?
+    
+    # for count, post in enumerate(posts):
+    #     post_lists.append("No.{}:".format(str(count)) +str(post)+"<br>")
+    #     return HttpResponse(post_lists)
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
